e0869f95d93d176e422d481a78fea4c3e61ee194
2kyoo <bladeoftown@yahoo.com>
2024-09-25 23:56:18 +0300

    Adding some routes

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {OptionsComponent} from "./components/options/options.component";
+import {StudentComponent} from "./components/student/student.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'register',component: OptionsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'control', component: OptionsComponent},
+  {path: 'students', component: StudentComponent},
+
+];
